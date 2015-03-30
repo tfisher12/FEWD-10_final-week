94d015257fbf6f22514d4bd85a569ef3cc6372f1
tfisher12 <timothymfisher@hotmail.com>
2015-03-30 21:10:52 +0100

    Added my project to Git

---
+++ ACTUAL MARK 2/index.js
@@ -0,0 +1,13 @@
+// var ('#primaryyv').css('color','red');
+
+var ('.social').css('font','132px');
+
+
+$(function () {
+$('.go-to-top').click(function () {
+$('html,body').animate({
+scrollTop: 0
+}, 500);
+return false;
+});
+});
